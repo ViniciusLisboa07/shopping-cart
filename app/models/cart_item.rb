52938defbea9aa13a4324e6ba86ef7f6a17f531d
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2025-09-21 17:53:40 -0300

    feat: add total_price attribute to CartItem model with validation and calculation before save

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -4,4 +4,13 @@ class CartItem < ApplicationRecord
 
   validates_presence_of :cart, :product, :quantity
   validates_numericality_of :quantity, greater_than: 0
+  validates_numericality_of :total_price, greater_than_or_equal_to: 0
+
+  before_save :calculate_total_price
+
+  private
+
+  def calculate_total_price
+    self.total_price = product.price * quantity
+  end
 end
